1f7e6d7b9925ec12c98a25fee3c74532abe6c5bd
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2021-11-28 15:04:46 +0200

    burger menu  start

--- int/js/common.js
+++ int/js/common.js
@@ -14,3 +14,20 @@ const swiper2 = new Swiper(".swiper-gallery-vertical", {
         clickable: true,
     },
 })
+// navigation
+const btnMenu = document.querySelector("#btn-show-menu");
+const hideMenu = document.querySelector("#btn-hide-menu");
+const body = document.querySelector("body");
+const menu = document.querySelector("#menu");
+
+btnMenu.onclick = function() {
+    btnMenu.classList.add("hide");
+    body.classList.add("overlay");
+    menu.classList.add("show");
+}
+
+hideMenu.onclick = function() {
+    btnMenu.classList.remove("hide");
+    body.classList.remove("overlay");
+    menu.classList.remove("show");
+}
