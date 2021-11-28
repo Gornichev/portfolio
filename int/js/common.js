d404bdc8092585abff1aafbe1c1a5ede825eef4b
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2021-11-28 10:42:11 +0200

    animation

---
+++ int/js/common.js
@@ -0,0 +1,16 @@
+// Swiper
+var swiper = new Swiper(".swiper-gallery", {
+    spaceBetween: 50,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+const swiper2 = new Swiper(".swiper-gallery-vertical", {
+    direction: "vertical",
+    spaceBetween: 50,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+})
