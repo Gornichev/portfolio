2d6b961bb865a77b88a6d34cf7138defce0ab670
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2021-11-29 17:25:23 +0200

    header-overlay

--- int/js/common.js
+++ int/js/common.js
@@ -1,3 +1,30 @@
+// Overlay-header
+
+var h = document.getElementById("overlay");
+var readout = document.getElementById("back");
+var stuck = false;
+var stickPoint = getDistance();
+
+function getDistance() {
+    var topDist = h.offsetTop;
+    return topDist;
+}
+
+window.onscroll = function(e) {
+    var distance = getDistance() - window.pageYOffset;
+    var offset = window.pageYOffset;
+    readout.innerHTML = stickPoint + '   ' + distance + '   ' + offset + '   ' + stuck;
+    if ( (distance <= 0) && !stuck) {
+        h.style.position = 'fixed';
+        h.style.top = '0px';
+        stuck = true;
+    } else if (stuck && (offset <= stickPoint)){
+        h.style.position = 'static';
+        stuck = false;
+    }
+}
+
+
 // Swiper
 var swiper = new Swiper(".swiper-gallery", {
     spaceBetween: 50,
